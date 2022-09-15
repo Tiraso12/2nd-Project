47cad77b1249ce3af85c7cd5207d1fcfb28712dc
Gonzalo Rodriguez <gonzaloostos02@gmail.com>
2022-09-15 19:53:06 -0400

    javascript for login and sign up form working

---
+++ controllers/htmlRoutes/login.js
@@ -0,0 +1,8 @@
+const router = require('..');
+
+const express = require('express').Router();
+
+
+router.get('/login', (req,res)=>{
+    res.sendFile('../../public/index.html');
+})
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
 DROP DATABASE IF EXISTS the_game_room;
 
 CREATE DATABASE the_game_room;
+
+USE the_game_room;
