b9338837abf8886ce183ca7a5594530333dd83d4
Bell <jacobbrister71@gmail.com>
2022-09-15 20:45:13 -0400

    added login route

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -55,6 +55,35 @@ router.post('/', (req, res) => {
         });
 });
 
+User.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(400).json({ message: 'Incorrect email or password' });
+                return;
+            }
+
+            const validPassword = dbUserData.checkPassword(req.body.password);
+
+            if(!validPassword) {
+                res.status(400).json({ message: 'Incorrect email or password' });
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: 'You are now logged in!' });
+            });
+        });
+});
+
 router.put('/:id', (req, res) => {
     User.update(req.body, {
         individualHooks: true,
