a101c486a7b108b8422c16a13d1a6db8c55df267
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-18 12:56:46 -0400

    Changes made to RockPaperScissors

--- public/javascript/RockPaperScissors.js
+++ public/javascript/RockPaperScissors.js
@@ -2,14 +2,30 @@ const computerChoiceDisplay = document.getElementById("computer-choice");
 const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
+let rockImage = "../images/Rock.png";
+let scissorImage = "../images/scissors.png";
+let paperImage = "../images/paper.png";
 let userChoice;
 let computerChoice;
 let result;
 
+let userChoiceImage;
 possibleChoices.forEach((possibleChoice) =>
   possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id;
-    userChoiceDisplay.innerHTML = userChoice;
+    console.log(userChoice);
+    if (userChoice === "rock") {
+      userChoiceImage = rockImage;
+    }
+    if (userChoice === "scissor") {
+      userChoiceImage = scissorImage;
+    }
+    if (userChoice === "paper") {
+      userChoiceImage = paperImage;
+    }
+    userChoiceDisplay.innerHTML = `
+    <img src="${userChoiceImage}"/> 
+    `;
     generateComputerChoice();
     getResult();
   })
@@ -19,37 +35,37 @@ function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
 
   if (randomNumber === 1) {
-    computerChoice = "rock";
+    computerChoice = rockImage;
   }
   if (randomNumber === 2) {
-    computerChoice = "scissors";
+    computerChoice = scissorImage;
   }
   if (randomNumber === 3) {
-    computerChoice = "paper";
+    computerChoice = paperImage;
   }
-  computerChoiceDisplay.innerHTML = computerChoice;
+  computerChoiceDisplay.innerHTML = `<img src="${computerChoice}"/> `;
 }
 
 function getResult() {
-  if (computerChoice === userChoice) {
+  if (computerChoice === userChoiceImage) {
     result = "Draw!";
   }
-  if (computerChoice === "rock" && userChoice === "paper") {
+  if (computerChoice === rockImage && userChoiceImage === paperImage) {
     result = "Username!";
   }
-  if (computerChoice === "rock" && userChoice === "scissors") {
+  if (computerChoice === rockImage && userChoiceImage === scissorImage) {
     result = "Computer!";
   }
-  if (computerChoice === "paper" && userChoice === "scissors") {
+  if (computerChoice === paperImage && userChoiceImage === scissorImage) {
     result = "Username!";
   }
-  if (computerChoice === "paper" && userChoice === "rock") {
+  if (computerChoice === paperImage && userChoiceImage === rockImage) {
     result = "Computer!";
   }
-  if (computerChoice === "scissors" && userChoice === "rock") {
+  if (computerChoice === scissorImage && userChoiceImage === rockImage) {
     result = "Username!";
   }
-  if (computerChoice === "scissors" && userChoice === "paper") {
+  if (computerChoice === scissorImage && userChoiceImage === paperImage) {
     result = "Computer!";
   }
   resultDisplay.innerHTML = result;
