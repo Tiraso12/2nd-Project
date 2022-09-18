0da7f968c2d86888e7fa9db35125a5fbbb7100fc
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-18 17:35:43 -0400

    More changes to make the flip a coin more interactive

--- public/javascript/flipACoin.js
+++ public/javascript/flipACoin.js
@@ -1,15 +1,25 @@
 //image shrinks horizontally heads then tails does the opposing to give it the appearance of flipping
+let imageContainer = document.querySelector(".imageContainer");
+let headsImage = `../images/heads.png`;
+let tailsImage = `../images/tails.png`;
 
 let start = document.getElementById("start");
 let result = document.getElementById("result");
 
 function Odds() {
   let coin = Math.random();
+  imageContainer.firstElementChild.classList.toggle("rotate");
 
   if (coin < 0.5) {
-    result.innerHTML = "You got Tails";
+    setTimeout(function () {
+      imageContainer.innerHTML = `<img src="${tailsImage}"/>`;
+      result.innerHTML = "You got Tails";
+    }, 1000);
   } else {
-    result.innerHTML = "You got Heads";
+    setTimeout(function () {
+      imageContainer.innerHTML = `<img src="${headsImage}"/>`;
+      result.innerHTML = "You got Heads";
+    }, 1000);
   }
 }
 
