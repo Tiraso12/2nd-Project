2225f6cf31ca4f7f2dd6c561e9d9b49c04d8c678
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-22 01:44:55 -0400

    Got all 3 handlebars pages to populate with local host

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -5,7 +5,31 @@ const { User } = require("../models");
 router.get("/", (req, res) => {
   console.log(req.session);
 
+  res.render("homepage", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+router.get("/headsOrTails", (req, res) => {
+  console.log(req.session);
+
   res.render("headsOrTails", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+router.get("/rockPaperScissors", (req, res) => {
+  console.log(req.session);
+
+  res.render("rockPaperScissors", {
+    loggedIn: req.session.loggedIn,
+  });
+});
+
+router.get("/highOrLow", (req, res) => {
+  console.log(req.session);
+
+  res.render("highOrLow", {
     loggedIn: req.session.loggedIn,
   });
 });
