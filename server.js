eab3e46755e8251bc5dc8e1f8d5fda5f4efda0e1
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-22 03:18:52 -0400

    working code to sign up and login

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const routes = require("./controllers");
+// const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 
 const app = express();
@@ -27,7 +27,7 @@ const sess = {
 
 app.use(session(sess));
 //new trial
-app.use(routes);
+// app.use(routes);
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
