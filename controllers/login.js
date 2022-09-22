a95eb13dac757bff7606a0243d8d46ac29b254ae
JoshuaAldrich <105825492+JoshuaAldrich@users.noreply.github.com>
2022-09-22 00:39:08 -0400

    trying to get other handlebars to work

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,7 @@
-const router = require('..');
+const router = require(".");
 
-const express = require('express').Router();
+const express = require("express").Router();
 
-
-router.get('/login', (req,res)=>{
-    res.sendFile('../../public/index.html');
-})
+router.get("/login", (req, res) => {
+  res.sendFile("../../public/index.html");
+});
